6676b65f56736904a759151c7fb1f2c753a651c1
soapun <leha5617441@gmail.com>
2022-02-14 15:43:05 +0300

    hotfix

--- task_01.py
+++ task_01.py
@@ -1,5 +1,12 @@
 def count_unique_symbols(it):
-    return len(set(it))
+    alphabet = [0 for i in range(52)]
+    res = 0
+    
+    for i in it:
+        res += not alphabet[ord(i) - ord('a')]
+        alphabet[ord(i) - ord('a')] = 1
+        
+    return res        
 
 if __name__ == "__main__":
     print(count_unique_symbols(input()))
